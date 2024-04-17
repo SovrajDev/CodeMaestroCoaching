29a0860f4de65d8681b2d7224e942d683ec5cad7
SovrajDev <161155048+SovrajDev@users.noreply.github.com>
2024-04-17 15:16:51 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose');
 //const username=process.env.MONGODB_USERNAME;
 //const password=process.env.MONGODB_PASSWORD;
 
- mongoose.connect("mongodb+srv://shreayas:Sovraj123@cluster0.xjxerxi.mongodb.net/registration-form").then(()=>{
+ mongoose.connect("mongodb+srv://shreayas:shreayasd123@cluster0.xjxerxi.mongodb.net/registration-form").then(()=>{
     console.log("connection established");
 }).catch((error)=>{
     console.log(error);
